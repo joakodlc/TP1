1aae903124fbf0e04cba28da099bbf591f3dea34
joakodlc <84253573+joakodlc@users.noreply.github.com>
2021-11-23 19:04:21 -0300

    Update TP.c

--- TP.c
+++ TP.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define RED        "\x1b[31m"
 #define YELLOW     "\x1b[33m"
-#define RESET      "\x1b[0m"
-#define BLUE       "\x1b[35m"
 #define GREEN        "\x1b[32m"
 #define CYAN        "\x1b[36m"
+#define RESET      "\x1b[0m"
 
 int main()
 {
